869048882973fd2430f13be8c095fa0692e8b2ed
Vincent Paeder <vpaeder@users.noreply.github.com>
2019-11-19 09:29:39 +0200

    Update tplink_sms.cxx
    minor formatting

--- tplink_sms.cxx
+++ tplink_sms.cxx
@@ -1,16 +1,16 @@
 /** \file tplink_sms.cxx
- *  This is a short example code that uses the TP-Link M7350 modem interface
- *  to send a SMS using the command line. This was tested to work with modem
- *  version 5 and firmware version 1.0.10.
- *  Author: Vincent Paeder
- *  License: GPL v3
+ *	This is a short example code that uses the TP-Link M7350 modem interface
+ *	to send a SMS using the command line. This was tested to work with modem
+ *	version 5 and firmware version 1.0.10.
+ *	Author: Vincent Paeder
+ *	License: GPL v3
  */
 
 #include <ctime>
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include <unistd.h>  
+#include <unistd.h>
 #include <curl/curl.h>
 #include <rapidjson/document.h>
 #include "rapidjson/writer.h"
@@ -18,9 +18,9 @@
 #include <openssl/md5.h>
 
 namespace tplink_sms {
-	
+
 	/** \enum AUTHENTICATOR_MODULE_ENUM
-	 *  \brief Command codes for TP-Link M7350 authenticator module.
+	 *	\brief Command codes for TP-Link M7350 authenticator module.
 	 */
 	enum AUTHENTICATOR_MODULE_ENUM {
 		/** Load */
@@ -34,67 +34,67 @@ namespace tplink_sms {
 		/** Perform page update */
 		AUTHENTICATOR_UPDATE = 4
 	};
-	
+
 	/** \enum MESSAGE_MODULE_ENUM
-	 *  \brief Command codes for TP-Link M7350 message module.
+	 *	\brief Command codes for TP-Link M7350 message module.
 	 */
 	enum MESSAGE_MODULE_ENUM {
 		/** Get message module configuration */
-    MESSAGE_GET_CONFIG = 0,
+		MESSAGE_GET_CONFIG = 0,
 		/** Set message module configuration */
-    MESSAGE_SET_CONFIG = 1,
+		MESSAGE_SET_CONFIG = 1,
 		/** Read stored message list */
-    MESSAGE_READ_MSG = 2,
+		MESSAGE_READ_MSG = 2,
 		/** Send message */
-    MESSAGE_SEND_MSG = 3,
+		MESSAGE_SEND_MSG = 3,
 		/** Save message */
-    MESSAGE_SAVE_MSG = 4,
+		MESSAGE_SAVE_MSG = 4,
 		/** Delete message(s) */
-    MESSAGE_DEL_MSG = 5,
+		MESSAGE_DEL_MSG = 5,
 		/** Mark message(s) as read */
-    MESSAGE_MARK_READ = 6,
+		MESSAGE_MARK_READ = 6,
 		/** Request current status */
-    MESSAGE_GET_SEND_STATUS = 7
+		MESSAGE_GET_SEND_STATUS = 7
 	};
-	
+
 	/** \enum SEND_MESSAGE_ENUM
-	 *  \brief Return codes for TP-Link M7350 'send message' function.
+	 *	\brief Return codes for TP-Link M7350 'send message' function.
 	 */
 	enum SEND_MESSAGE_ENUM {
 		/** Message sent and saved successfully */
-	   SMS_SEND_SUCCESS_SAVE_SUCCESS = 0,
+		 SMS_SEND_SUCCESS_SAVE_SUCCESS = 0,
 		/** Message sent successfully but not saved */
-	   SMS_SEND_SUCCESS_SAVE_FAIL = 1,
+		 SMS_SEND_SUCCESS_SAVE_FAIL = 1,
 		/** Message save successfully but not sent */
-	   SMS_SEND_FAIL_SAVE_SUCCESS = 2,
+		 SMS_SEND_FAIL_SAVE_SUCCESS = 2,
 		/** Message not sent and not saved */
-	   SMS_SEND_FAIL_SAVE_FAIL = 3,
+		 SMS_SEND_FAIL_SAVE_FAIL = 3,
 		/** Currently sending */
-	   SMS_SENDING = 4
+		 SMS_SENDING = 4
 	};
-	
+
 	/** \fn std::string get_md5_hash(std::string & passwd)
-	 *  \brief Generates a MD5 hash of the given string.
-	 *  \param str : string to compute the MD5 hash from.
-	 *  \returns a string containing the MD5 hash of the input string.
+	 *	\brief Generates a MD5 hash of the given string.
+	 *	\param str : string to compute the MD5 hash from.
+	 *	\returns a string containing the MD5 hash of the input string.
 	 */
 	std::string get_md5_hash(std::string & str) {
-    unsigned char digest[16];
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, str.c_str(), str.size());
-    MD5_Final(digest, &ctx);
-    char md_string[33];
-    for (int i = 0; i < 16; i++)
-      sprintf(&md_string[i*2], "%02x", (unsigned int)digest[i]);
-	
+		unsigned char digest[16];
+		MD5_CTX ctx;
+		MD5_Init(&ctx);
+		MD5_Update(&ctx, str.c_str(), str.size());
+		MD5_Final(digest, &ctx);
+		char md_string[33];
+		for (int i = 0; i < 16; i++)
+			sprintf(&md_string[i*2], "%02x", (unsigned int)digest[i]);
+
 		return std::string(md_string);
 	}
-	
+
 	/** \fn std::string hexdigest(std::string & str)
-	 *  \brief Generates a hex digest of the given string.
-	 *  \param str : string to digest.
-	 *  \returns a string containing a hexadecimal representation of the input string.
+	 *	\brief Generates a hex digest of the given string.
+	 *	\param str : string to digest.
+	 *	\returns a string containing a hexadecimal representation of the input string.
 	 */
 	std::string hexdigest(std::string & str) {
 		std::ostringstream res;
@@ -103,25 +103,25 @@ namespace tplink_sms {
 			res << str[i];
 		return res.str();
 	}
-	
+
 	/* CURL error buffer */
 	static char errorBuffer[CURL_ERROR_SIZE];
-	
+
 	/* CURL writer callback (collects server response) */
 	static int writer(char *data, size_t size, size_t nmemb, std::string *writerData) {
 		if ( writerData == NULL )
 			return 0;
-	
+
 		writerData->append( data, size*nmemb );
-		
+
 		return size * nmemb;
 	}
-	
+
 	/** \fn rapidjson::Document post_data(std::string & url, std::string & data)
-	 *  \brief Requests given URL with given data using POST method.
-	 *  \param url : URL to request
-	 *  \param data : data string to join to the request
-	 *  \returns a rapidjson::Document object containing the server response parsed as JSON.
+	 *	\brief Requests given URL with given data using POST method.
+	 *	\param url : URL to request
+	 *	\param data : data string to join to the request
+	 *	\returns a rapidjson::Document object containing the server response parsed as JSON.
 	 */
 	rapidjson::Document post_data(std::string & url, std::string & data) {
 		CURL *conn = NULL;
@@ -129,50 +129,50 @@ namespace tplink_sms {
 		rapidjson::Document d;
 		conn = curl_easy_init();
 		if ( conn == NULL ) return d;
-	
+
 		// set error buffer for CURL
 		//std::string errorBuffer;
 		code = curl_easy_setopt( conn, CURLOPT_ERRORBUFFER, & errorBuffer );
 		if ( code != CURLE_OK ) return d;
-	
+
 		// set data buffer for CURL
 		std::string buffer;
 		code = curl_easy_setopt( conn, CURLOPT_WRITEDATA, & buffer );
 		if ( code != CURLE_OK ) return d;
-	
+
 		// set data writer function
 		code = curl_easy_setopt( conn, CURLOPT_WRITEFUNCTION, writer );
 		if ( code != CURLE_OK ) return d;
-	
+
 		// set URL
 		code = curl_easy_setopt( conn, CURLOPT_URL, url.c_str() );
 		if ( code != CURLE_OK ) return d;
-	
-	  // set POST data
+
+		// set POST data
 		curl_easy_setopt( conn, CURLOPT_POSTFIELDSIZE, data.size() );
-	  curl_easy_setopt( conn, CURLOPT_POSTFIELDS, data.c_str() );
-	
+		curl_easy_setopt( conn, CURLOPT_POSTFIELDS, data.c_str() );
+
 		// access page
 		code = curl_easy_perform( conn );
 		if ( code != CURLE_OK ) return d;
 		d.Parse(buffer.c_str());
-		
+
 		curl_easy_cleanup( conn );
 		return d;
-	
+
 	}
-	
+
 	/** \fn bool send_sms(std::string modem_address, std::string passwd, std::string phone_number, std::string message)
-	 *  \brief Sends a SMS through the TP-Link M7350 interface.
-	 *  \param modem_address : modem IP address or DNS
-	 *  \param passwd : modem admin password
-	 *  \param phone_number : recipient number
-	 *  \param message : text message to send
-	 *  \returns true if successful, false otherwise.
+	 *	\brief Sends a SMS through the TP-Link M7350 interface.
+	 *	\param modem_address : modem IP address or DNS
+	 *	\param passwd : modem admin password
+	 *	\param phone_number : recipient number
+	 *	\param message : text message to send
+	 *	\returns true if successful, false otherwise.
 	 */
 	bool send_sms(std::string modem_address, std::string passwd, std::string phone_number, std::string message) {
 		/* Working principle of TP-Link M7350v5 web interface (firmware v.1.0.10)
-				First contact the server on cgi-bin/auth_cgi to obtain a password salt:
+				Contact server on cgi-bin/auth_cgi to obtain a password salt:
 					Parameters to pass: {"module":"authenticator","action":0}
 					Returned data: {"authedIP":"0.0.0.0","nonce":"password salt",result:1}
 				Authenticate:
@@ -186,16 +186,18 @@ namespace tplink_sms {
 					Returned data: {"cause":0,"result":0}
 		*/
 		namespace rj = rapidjson;
+		// URLs of modem interface
 		std::string auth_url = "http://" + modem_address + "/cgi-bin/auth_cgi";
 		std::string web_url = "http://" + modem_address + "/cgi-bin/web_cgi";
-		rj::Document d;
-		std::string req_data;
 		
-		/* Get password salt */
+		std::string req_data; // for POST request data
+		rj::Document d; // for server replies
+
+		/* get password salt */
 		{
 			// build JSON request object
 			rj::StringBuffer s;
-	    rj::Writer<rj::StringBuffer> writer(s);
+			rj::Writer<rj::StringBuffer> writer(s);
 			rj::Document req_json;
 			req_json.SetObject();
 			req_json.AddMember("module", "authenticator", req_json.GetAllocator());
@@ -209,15 +211,16 @@ namespace tplink_sms {
 		// check that server returned a valid password salt
 		if (!d.HasMember("nonce")) return false;
 		if (!strcmp(d["nonce"].GetString(),"")) return false;
-		// Log in
+		
+		/* log in */
 		{
 			// create salted password MD5 digest
 			std::string spwd = passwd+":"+d["nonce"].GetString();
 			std::string md5_hash = get_md5_hash(spwd);
-		  std::string auth_digest =  hexdigest(md5_hash);
+			std::string auth_digest =	hexdigest(md5_hash);
 			// build JSON request object
 			rj::StringBuffer s;
-	    rj::Writer<rj::StringBuffer> writer(s);
+			rj::Writer<rj::StringBuffer> writer(s);
 			rj::Document req_json;
 			req_json.SetObject();
 			req_json.AddMember("module", "authenticator", req_json.GetAllocator());
@@ -234,8 +237,8 @@ namespace tplink_sms {
 		if (!d.HasMember("token")) return false;
 		if (!strcmp(d["token"].GetString(),"")) return false;
 		std::string token = d["token"].GetString();
-	
-		// send message
+
+		/* send message */
 		{
 			// create time stamp
 			time_t now = std::time( 0 );
@@ -244,7 +247,7 @@ namespace tplink_sms {
 			std::sprintf( timestamp, "%04d,%02d,%02d,%02d,%02d,%02d", 1900+t_now->tm_year, t_now->tm_mon, t_now->tm_mday, t_now->tm_hour, t_now->tm_min, t_now->tm_sec );
 			// build JSON request object
 			rj::StringBuffer s;
-	    rj::Writer<rj::StringBuffer> writer(s);
+			rj::Writer<rj::StringBuffer> writer(s);
 			rj::Document req_json;
 			req_json.SetObject();
 			req_json.AddMember("module","message",req_json.GetAllocator());
@@ -263,14 +266,15 @@ namespace tplink_sms {
 			// serialize
 			req_json.Accept(writer);
 			req_data = s.GetString();
-			
+
 			d = post_data(web_url, req_data);
 		}
-		// wait until message has been sent
+		
+		/* wait until message has been sent */
 		{
 			// build JSON request object
 			rj::StringBuffer s;
-	    rj::Writer<rj::StringBuffer> writer(s);
+			rj::Writer<rj::StringBuffer> writer(s);
 			rj::Document req_json;
 			req_json.SetObject();
 			req_json.AddMember("module","message",req_json.GetAllocator());
@@ -291,12 +295,12 @@ namespace tplink_sms {
 /* main function - returns 0 if execution went fine, 1 otherwise */
 int main( int argc, char** argv ) {
 	using namespace tplink_sms;
-	
+
 	// flags indicating whether arguments have been set
 	bool address_set = false, pw_set = false, number_set = false, message_set = false;
 	// argument values
 	std::string address, passwd, phone_number, message;
-	
+
 	// parse command line for arguments
 	int opt;
 	while ( ( opt = getopt ( argc, argv, "ha:p:n:m:" ) ) != -1 ) {
@@ -306,22 +310,22 @@ int main( int argc, char** argv ) {
 				std::cout << argv[0] << " -a modem_address -p password -n phone_number -m message" << std::endl;
 				std::cout << argv[0] << " -h" << std::endl;
 				break;
-			
+
 			case 'a':
 				address = optarg;
 				address_set = true;
 				break;
-			
+
 			case 'p':
 				passwd = optarg;
 				pw_set = true;
 				break;
-			
+
 			case 'n':
 				phone_number = optarg;
 				number_set = true;
 				break;
-			
+
 			case 'm':
 				message = optarg;
 				message_set = true;
@@ -333,6 +337,6 @@ int main( int argc, char** argv ) {
 		std::cout << "Type " << argv[0] << " -h for help" << std::endl;
 		return 1;
 	}
-	
+
 	return send_sms(address, passwd, phone_number, message) ? 0 : 1;
 }
